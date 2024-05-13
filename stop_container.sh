fa40cb67d5d57a47b4ea3097d26f5a6315c41573
Srinath25 <sritharun256@gmail.com>
2024-05-13 02:24:52 -0400

    Final Commit to test the complete CICD

--- stop_container.sh
+++ stop_container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#set -e
+set -e
 
 # Stop the running container (if any)
-#containerid=$(docker ps | awk 'NR==2{print $1}')
-#if [ ! -z "$containerid" ]; then
-#    docker rm -f $containerid
-#else
-#    echo "No running containers found."
-#fi
+containerid=$(docker ps | awk 'NR==2{print $1}')
+if [ ! -z "$containerid" ]; then
+    docker rm -f $containerid
+else
+    echo "No running containers found."
+fi
